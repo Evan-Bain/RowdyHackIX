e97eb2db5eb79f6d3b3609eba8ad08e131560c33
Evan-Bain <evanjbain@gmail.com>
2024-02-24 12:44:50 -0600

    Google Maps Fragment added

--- app/src/main/java/com/example/rowdyhacks/MainActivity.kt
+++ app/src/main/java/com/example/rowdyhacks/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.rowdyhacks
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.FragmentManager
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.frag_container_view, MapsFragment::class.java, null)
+            .addToBackStack(null)
+            .commit()
     }
 }
